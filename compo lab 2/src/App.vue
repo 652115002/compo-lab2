86549b24eedc59aaed9f0c2735a8252ae3fda802
652115002 <153349262+652115002@users.noreply.github.com>
2024-07-11 21:37:18 +0700

    4.4

---
+++ compo lab 2/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import EventCard from './components/EventCard.vue'
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+#layout {
+  font-family: Avinir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
