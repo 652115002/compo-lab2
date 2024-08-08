136595899b2bf7a39bd5931913a5bef1c2a41844
652115002 <153349262+652115002@users.noreply.github.com>
2024-08-08 22:15:02 +0700

    lab4

--- compo lab 2/src/router/index.ts
+++ compo lab 2/src/router/index.ts
@@ -7,12 +7,14 @@ import EventDetailView from '@/views/event/EventDetailView.vue'
 import EventEditView from '@/views/event/EventEditView.vue'
 import EventRegisterView from '@/views/event/EventRegisterView.vue'
 import EventLayoutView from '@/views/event/EventLayout.vue'
-import NotFoundView from '@/views/NotFoundView.vue' 
+import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
-
+import nProgress from 'nprogress'
+import EventService from '@/services/EventServices'
+import { useEventStore } from '@/stores/event'
 
 export function createAppRouter(pageLimit: (number | null)[]) {
-  return createRouter({
+  const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
       {
@@ -36,27 +38,42 @@ export function createAppRouter(pageLimit: (number | null)[]) {
       },
       {
         path: '/event/:id',
-        name: 'event-layout',
+        name: 'event-layout-view',
         component: EventLayoutView,
         props: true,
+        beforeEnter: (to) => {
+          const id = parseInt(to.params.id as string)
+          const eventStore = useEventStore()
+          return EventService.getEventById(id)
+            .then((response) => {
+              eventStore.setEvent(response.data)
+            })
+            .catch((error) => {
+              if (error.response && error.response.status === 404) {
+                return {
+                  name: '404-resource-view',
+                  params: { resourse: 'event' }
+                }
+              }
+            })
+        },
         children: [
           {
             path: '',
-            name: 'event-detail',
+            name: 'event-detail-view',
             component: EventDetailView,
-            props:true
+            props: true
           },
           {
             path: 'edit',
-            name: 'event-edit',
-            props:true,
+            name: 'event-edit-view',
+            props: true,
             component: EventEditView
-            
           },
           {
             path: 'register',
-            name: 'event-register',
-            props:true,
+            name: 'event-register-view',
+            props: true,
             component: EventRegisterView
           }
         ]
@@ -73,25 +90,34 @@ export function createAppRouter(pageLimit: (number | null)[]) {
         props: true,
         component: EventRegisterView
       },
-
-      {
-        path: '/404/:resource',
-        name: '404-resource',
-        component: NotFoundView,
-        props: true
-      },
-      {
-        path: '/:catchAll(.*)',
-        name: 'not-found',
-        component: NotFoundView
-      },
-
-      {
-        path: '/network-error',
-        name: 'network-error',
-        component: NetworkErrorView
-      },
-      
-    ]
-  })
+    {
+      path: '/404/:resource',
+      name: '404-resource',
+      component: NotFoundView,
+      props: true
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+    },
+    {
+      path: '/network-error',
+      name: 'network-error',
+      component: NetworkErrorView
+    }
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else return { top: 0 }
+  }
+})
+router.beforeEach(() => {
+  nProgress.start()
+})
+router.afterEach(() => {
+  nProgress.done()
+})
+return router
 }
